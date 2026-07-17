2c2384a2ddd3278e1b8071e342a1e848fd47e236
Clintin dos Sites <clintindossites@gmail.com>
2026-07-17 15:08:12 -0300

    feat: add shadcn initial setup and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <></>;
-}
+const HomePage = () => {
+  return <h1>Iluminado Café</h1>;
+};
+
+export default HomePage;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500 ">Pagina do produto</h1>
+      <Button>Iluminado Café</Button>
+      <Input></Input>
+    </div>
+  );
+};
+
+export default ProductPage;
