bcc8056a25866964912f9c3993f94d1a0b0feb0f
Clintin dos Sites <clintindossites@gmail.com>
2026-07-18 17:48:12 -0300

    feat: add restautant menu page with categories and products

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "u9a6wmr3as.ufs.sh",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
