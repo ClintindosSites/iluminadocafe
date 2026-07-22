6397574e5e3eb031fb5607dbc58a83d7a19f5b41
Clintin dos Sites <clintindossites@gmail.com>
2026-07-22 11:28:07 -0300

    feat: ensure cart product quantity can be increased

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -14,6 +14,7 @@ export interface ICartContext {
     toggleCart: () => void
     addProduct: (product: CartProduct) => void
     decreaseProductQuantity: (productId: string) => void
+     increaseProductQuantity: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -21,7 +22,8 @@ export const CartContext = createContext<ICartContext>({
     products: [],
     toggleCart: () => {},
     addProduct: () => {},
-    decreaseProductQuantity: () => {}
+    decreaseProductQuantity: () => {},
+    increaseProductQuantity: () => {}
 })
 
 export const CartProvider = ({children} : {children: ReactNode}) => {
@@ -50,7 +52,7 @@ export const CartProvider = ({children} : {children: ReactNode}) => {
         })
      
     }
-    const decreaseCartProductQuantity = (productId: string) => {
+    const decreaseProductQuantity = (productId: string) => {
       setProducts(prevProducts => {
         return prevProducts.map(prevProduct => {
           if (prevProduct.id !== productId) {
@@ -64,12 +66,25 @@ export const CartProvider = ({children} : {children: ReactNode}) => {
         })
       })
     }
+
+    const increaseProductQuantity = (productId: string) => {
+       setProducts((prevProducts) => {
+        return prevProducts.map((prevProduct) => {
+          if (prevProduct.id !== productId) {
+            return prevProduct
+          }
+         
+          return {...prevProduct, quantity: prevProduct.quantity + 1}
+        })
+      })
+    }
   return <CartContext.Provider value={{
     isOpen: isOpen,
     products: products,
     toggleCart: toggleCart,
     addProduct: addProduct,
-    decreaseProductQuantity: decreaseCartProductQuantity
+    decreaseProductQuantity: decreaseProductQuantity,
+    increaseProductQuantity: increaseProductQuantity
   }}>
 {children}
   </CartContext.Provider>
